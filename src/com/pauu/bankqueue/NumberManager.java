f283b41f40784f97d61dcd7204e2faf6e04e5beb
peng.xing <southeast_px@163.com>
2016-09-02 13:57:18 +0800

    银行业务调度系统demo

---
+++ src/com/pauu/bankqueue/NumberManager.java
@@ -0,0 +1,22 @@
+package com.pauu.bankqueue;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * 号码管理器
+ * @author peng.xing
+ *
+ */
+public class NumberManager {
+	private int lastNumber = 1;
+	private List<Integer> queueNumber = new ArrayList<Integer>();
+	//生成号码给客户
+	public synchronized int generateNewNumber(){
+		queueNumber.add(lastNumber);
+		return lastNumber++;
+	}
+	//排队窗口取号
+	public synchronized int fetchServiceNumber(){
+		return queueNumber.remove(0);
+	}
+}
